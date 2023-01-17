27c09c837cde374ea7f928ffb65b5f136ed2adb1
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-01-17 15:34:02 +0100

    22 - using useDebounce custom hook inside the SearchBar component

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useDebounce } from "../../hooks/useDebounce";
 
-const SearchbarInput = () => {
+interface Props {
+  onChange?: (searchInput?: string) => void;
+}
+
+const SearchbarInput = ({ onChange = () => null }: Props) => {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const debouncedSearchInput = useDebounce<string>(searchInput, 500);
+
+  const handleChangeSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(() => e.target.value);
+  };
+
+  useEffect(() => {
+    onChange?.(debouncedSearchInput);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchInput]);
+
   return (
     <div className="w-[800] h-24 flex justify-center items-center">
       <input
         type="text"
         className="w-96 h-12 p-2 border-2 border-blue-600 rounded-2xl"
+        onChange={handleChangeSearchInput}
       />
     </div>
   );
