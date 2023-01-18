0655dd6550b72faacc73eebbf61e3ef826a0d9ff
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-01-19 00:57:08 +0100

    24 - adjusting fetchStatistics service to take searchValue params

--- src/services/statistics.ts
+++ src/services/statistics.ts
@@ -2,9 +2,12 @@ import request from "../lib/request";
 import { StatisticsResponse } from "../models";
 import env from "ts-react-dotenv";
 
-const fetchStatistics = async (): Promise<StatisticsResponse> => {
+const fetchStatistics = async (
+  searchValue: string = ""
+): Promise<StatisticsResponse> => {
   try {
-    const response = await request.get(env.API_BASE_URL); // here would be /statistics
+    const url = `${env.API_BASE_URL}?q=${searchValue}`;
+    const response = await request.get(url); // here would be /statistics
 
     return response.data;
   } catch (e) {
