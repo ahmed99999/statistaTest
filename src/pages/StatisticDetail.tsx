bc8a902754512f34f4a2242e2fb968487b281c5c
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-01-19 02:09:37 +0100

    29 - initial StatisticDetail component ( no style  )

--- src/pages/StatisticDetail.tsx
+++ src/pages/StatisticDetail.tsx
@@ -1,3 +1,42 @@
-export default function T1() {
-  return <div>t</div>;
-}
+import React from "react";
+import { useStatistic } from "../hooks/useStatistic";
+import { useParams } from "react-router-dom";
+
+const onSuccess = () => {
+  console.log("fetching statistics");
+};
+
+const onError = () => {
+  console.log("encountering errors while fetching statistics");
+};
+
+const StatisticDetail = () => {
+  let { statisticId = "" } = useParams();
+
+  const {
+    isError,
+    isLoading,
+    data: statistic,
+  } = useStatistic(parseInt(statisticId), onSuccess, onError);
+
+  if (isLoading) {
+    return <div>Loading Statistic...</div>;
+  }
+
+  if (isError) {
+    return <div>Error Loading Statistic...</div>;
+  }
+
+  if (!statistic) {
+    return <div>this statistic was not found</div>;
+  }
+
+  return (
+    <div className="">
+      {statistic.title}
+      {statistic.description}
+    </div>
+  );
+};
+
+export default StatisticDetail;
