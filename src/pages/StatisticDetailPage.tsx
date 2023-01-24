cc597cb6b39f3e767885e3cf60a0d1dd05dfda1a
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-01-24 19:41:41 +0100

    45 - Statistic Detail Page

--- src/pages/StatisticDetailPage.tsx
+++ src/pages/StatisticDetailPage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useStatistic } from "../hooks/useStatistic";
 import { useParams } from "react-router-dom";
+import { getDateFormatted } from "../utils/dateFormat";
+import Image from "../components/Image";
+import { BsArrowRight } from "react-icons/bs";
 
 const StatisticDetailPage = () => {
   let { statisticId = "" } = useParams();
@@ -23,10 +26,50 @@ const StatisticDetailPage = () => {
     return <div>this statistic was not found</div>;
   }
 
+  const statisticDate = getDateFormatted(statistic.date);
+  const premium = statistic.premium ? "Yes" : "No";
+  const teaserImage = statistic.teaser_image_urls[1];
+
   return (
-    <div className="">
-      {statistic.title}
-      {statistic.description}
+    <div className="statisticDetail">
+      <h1 className="text-blue-900 text-3xl">{statistic.title}</h1>
+
+      <div className="flex gap-3 w-full mt-5">
+        <Image className="statisticDetailImage" src={statistic.image_url} />
+        <div className="statisticDetailInfo">
+          <div>
+            <h2 className="statisticDetailInfoItem">Release date: </h2>
+            <span>{statisticDate}</span>
+          </div>
+
+          <div>
+            <h2 className="statisticDetailInfoItem">Premium Statistic :</h2>
+            {premium}
+          </div>
+
+          {teaserImage && (
+            <div>
+              <h2 className="statisticDetailInfoItem">Teaser image:</h2>
+
+              <Image
+                src={teaserImage.src}
+                width={teaserImage.width}
+                className="teaserImage"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="statisticDetailAdditionalInfo">
+        <h1 className="statisticDetailDescription">Description: </h1>
+        <span>Published on {statisticDate}</span>
+        <span>{statistic.description}</span>
+        <span>{statistic.subject}</span>
+        <span className="link text-lg">
+          <BsArrowRight /> For more Info
+        </span>
+      </div>
     </div>
   );
 };
