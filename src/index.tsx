34fc3eb028236508bef1c17e931cf60119cf1c53
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-01-24 19:42:18 +0100

    46 - Removing ReactQueryDevtools

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query-devtools";
+// import { ReactQueryDevtools } from "react-query-devtools";
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <App />
-                <ReactQueryDevtools />
-            </QueryClientProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        {/*<ReactQueryDevtools />*/}
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
